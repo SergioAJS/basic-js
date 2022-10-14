f2c63c2f27384837cd47e66f2f0145f3f0fa04f8
SergioAJS <SergFanMadrid@gmail.com>
2022-10-14 14:40:46 +0500

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(arr)) {
+    let modArr = [...arr];
+    for (let i = 0; i < modArr.length; i++) {
+      if (typeof modArr[i] === 'string') {
+        console.log(modArr[i]);
+        if (modArr[i] === '--discard-next') {
+          if (modArr[i + 1]) {
+            modArr.splice(i + 1, 1);
+          } else {
+            modArr.splice(i, 1);
+          }
+        }
+        if (modArr[i] === '--discard-prev') {
+          if (modArr[i - 1] && typeof modArr[i - 1] !== 'string') {
+            modArr.splice(i - 1, 1);
+            modArr.splice(i - 1, 1);
+          } else if (typeof modArr[i - 1] === 'string') {
+            modArr.splice(i, 1);
+            modArr.splice(i - 1, 1);
+          } else {
+            modArr.splice(i, 1);
+          }
+        }
+        if (modArr[i] === '--double-next') {
+          if (modArr[i + 1]) {
+            modArr.splice(i + 1, 0, modArr[i + 1]);
+            modArr.splice(i, 1);
+          } else {
+            modArr.splice(i, 1);
+          }
+        }
+        if (modArr[i] === '--double-prev') {
+          if (modArr[i - 1] && typeof modArr[i - 1] !== 'string') {
+            modArr.splice(i - 1, 0, modArr[i - 1]);
+            modArr.splice(i + 1, 1);
+          } else if (typeof modArr[i - 1] === 'string') {
+            modArr.splice(i, 1);
+            modArr.splice(i - 1, 1);
+          } else {
+            modArr.splice(i, 1);
+          }
+        }
+      }
+    }
+    return modArr;
+  } else {
+    throw new Error (`'arr' parameter must be an instance of the Array!`);
+  }
 }
 
 module.exports = {
